b838c30d0f65d0b3b280783ef61a180cf2961a63
greygrudge <mahathiraleef59@gmail.com>
2026-02-03 15:57:33 +0600

    Update vite.config.js

--- vite.config.js
+++ vite.config.js
@@ -4,5 +4,5 @@ import react from '@vitejs/plugin-react'
 // https://vitejs.dev/config/
 export default defineConfig({
   plugins: [react()],
-  base: '/elenorm_main/'   // VERY IMPORTANT — matches your repo name
+  base: '/elenorm_main/'  // MUST match your repo name exactly
 })
