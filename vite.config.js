26c09bb0da68a6e76adbd2c2646aee47f70db813
greygrudge <mahathiraleef59@gmail.com>
2026-02-03 15:51:49 +0600

    Create vite.config.js

---
+++ vite.config.js
@@ -0,0 +1,8 @@
+import { defineConfig } from 'vite'
+import react from '@vitejs/plugin-react'
+
+// https://vitejs.dev/config/
+export default defineConfig({
+  plugins: [react()],
+  base: '/elenorm_main/'   // VERY IMPORTANT — matches your repo name
+})
